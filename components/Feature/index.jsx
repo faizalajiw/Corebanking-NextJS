2178d6050bd6a8905662a0c5daa81c10141fd648
zaidzu <Cmohammadzaidan20112000@gmail.com>
2022-08-19 10:59:05 +0700

    add content at Data.js

--- components/Feature/index.jsx
+++ components/Feature/index.jsx
@@ -17,14 +17,40 @@ export const Feature = () => {
         </div>
         <div className="container flex ">
             <div className="container px-10">
-                <h1 className="text-2xl mb-10">Credit Scoring</h1>
+                <h1 className="text-2xl mb-10">Mobile Banking</h1>
                 <p className="mb-10 font-light">
-                Pelayanan untuk mengecek tingkat kelayakan kredit untuk mendapatkan produk seperti Kredit Tanpa Agunan (KTA), Kredit Pemilikan Rumah (KPR), kartu kredit, dan lain-lain.
+                Cek saldo rekening, transfer dana, dan bayar tagihan Anda dengan lebih mudah dan nyaman melalui 
+                M-Banking CREDIS.
                 </p>
                 <button className="bg-green-400 rounded px-4 py-2 text-xs">Selengkapnya</button>
             </div>
             <div className="container">
-                <img src="img/credit scoring.png" alt="" className=" w-96" />
+                <img src="img/mobile banking.png" alt="" className=" w-96" />
+            </div>
+        </div>
+        <div className="container flex ">
+            <div className="container flex">
+                <img src="img/kyc.png" alt="" className=" w-96" />
+            </div>
+            <div className="container px-10">
+                <h1 className="text-2xl mb-10">Verifikasi KYC</h1>
+                <p className="mb-10 font-light">
+                Identifikasi, verifikasi, dan pemantauan transaksi nasabah demi melindungi keamanan data dan kenyamanan layanan finansial Anda.
+                </p>
+                <button className="bg-green-400 rounded px-4 py-2 text-xs">Selengkapnya</button>
+            </div>
+        </div>
+        <div className="container flex ">
+            <div className="container px-10">
+                <h1 className="text-2xl mb-10">Laporan Terperinci</h1>
+                <p className="mb-10 font-light">
+                Semua riwayat transfer, mutasi, dan sistem keuangan Anda direkap secara terperinci dan bisa 
+                di unduh.
+                </p>
+                <button className="bg-green-400 rounded px-4 py-2 text-xs">Selengkapnya</button>
+            </div>
+            <div className="container">
+                <img src="img/laporan terperinci.png" alt="" className=" w-96" />
             </div>
         </div>
     </div>
