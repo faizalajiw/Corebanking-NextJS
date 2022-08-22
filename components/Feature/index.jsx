3ea346da8a1664b61c75982d8f37d2a6c99784b1
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2022-08-22 19:35:40 +0700

    responsive testicard, subs, footer

--- components/Feature/index.jsx
+++ components/Feature/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export const Feature = () => {
+export const Feature = ({id, image, header, title}) => {
   return (
-    <div className="container px-32 flex flex-col gap-10">
-        <div className="container flex ">
-            <div className="container">
-                <img src="img/credit scoring.png" alt="" className=" w-96" />
+    <div className="container flex flex-col gap-10 px-10 md:px-32">
+        <div className="container flex flex-col md:flex-row">
+            <div className="container flex justify-center md:justify-start">
+                <img src="img/credit scoring.png" alt="" className="max-w-xs md:w-96" />
             </div>
-            <div className="container px-10">
+            <div className="container px-10 text-center md:text-left">
                 <h1 className="text-2xl mb-10">Credit Scoring</h1>
                 <p className="mb-10 font-light">
                 Pelayanan untuk mengecek tingkat kelayakan kredit untuk mendapatkan produk seperti Kredit Tanpa Agunan (KTA), Kredit Pemilikan Rumah (KPR), kartu kredit, dan lain-lain.
@@ -15,16 +15,19 @@ export const Feature = () => {
                 <button className="bg-green-400 rounded px-4 py-2 text-xs">Selengkapnya</button>
             </div>
         </div>
-        <div className="container flex ">
-            <div className="container px-10">
+        <div className="container flex flex-col md:flex-row">
+            <div className="container flex justify-center md:justify-start md:hidden">
+                <img src="img/credit scoring.png" alt="" className="max-w-xs md:w-96" />
+            </div>
+            <div className="container px-10 text-center md:text-left">
                 <h1 className="text-2xl mb-10">Credit Scoring</h1>
                 <p className="mb-10 font-light">
                 Pelayanan untuk mengecek tingkat kelayakan kredit untuk mendapatkan produk seperti Kredit Tanpa Agunan (KTA), Kredit Pemilikan Rumah (KPR), kartu kredit, dan lain-lain.
                 </p>
                 <button className="bg-green-400 rounded px-4 py-2 text-xs">Selengkapnya</button>
             </div>
-            <div className="container">
-                <img src="img/credit scoring.png" alt="" className=" w-96" />
+            <div className="container hidden md:flex">
+                <img src="img/credit scoring.png" alt="" className="max-w-xs md:w-96" />
             </div>
         </div>
     </div>
--- components/Subs/index.jsx
+++ components/Subs/index.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export const Subs = () => {
   return (
-    <div className="bg-bluecredis flex px-32 pt-5 ">
-        <div className="container flex-[160%]">
+    <div className="bg-bluecredis flex px-10 pt-5 md:px-32">
+        <div className="container flex-[160%] text-center md:text-left">
             <h1 className="font-bold text-3xl pb-1">Berlangganan kepada kami</h1>
             <p className="pb-6">Dapatkan berbagai promosi dan informasi lainnya</p>
-            <form action="" className="relative">
-                <input type="text" placeholder="Email" className=" absolute text-gray-400 bg-white px-5 text-sm rounded w-[460px] h-12"/>
-                <button className="px-5 py-2 rounded bg-bluecredis absolute left-[330px] top-[6px] text-sm">Berlangganan</button>
+            <form action="" className=" flex justify-center content-center gap-3 md:relative md:justify-start md:content-start">
+                <input type="text" placeholder="Email" className="text-gray-400 bg-white px-5 text-sm rounded h-12 w-72 md:absolute md:w-[460px]"/>
+                <button className="px-5 py-3 md:py-2 rounded bg-green-400  top-[6px] left-[158px] text-sm md:absolute md:left-[330px]">Berlangganan</button>
             </form>
         </div>
-        <div className="container ">
+        <div className="container hidden md:flex">
             <img src="img/subscribe.png" alt="" className=" w-80"/>
         </div>
     </div>
