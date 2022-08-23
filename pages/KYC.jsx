e612ec46b360dccb429359582d7b1217189a319c
faizalajiw <faizalajiwibowo7@gmail.com>
2022-08-23 11:31:35 +0700

    adding fitur KYC & edit Credit Score

--- pages/KYC.jsx
+++ pages/KYC.jsx
@@ -4,14 +4,14 @@ import { Subs } from "../components/Subs";
 import { Footer } from "../components/Footer";
 import { UilCheckCircle } from '@iconscout/react-unicons'
 
-export default function CreditScoring() {
+export default function KYC() {
   return (
     <>
       <Navbar />
       <div className="bg-white">
         <div className="py-24">
           <div className="font-semibold text-center text-bluecredis text-4xl">
-            {fitur[0].title}
+            {fitur[1].title}
           </div>
         </div>
 
@@ -19,11 +19,11 @@ export default function CreditScoring() {
           {/* Col 1 */}
           <div>
             <div className="text-2xl font-medium text-[#1E1E1E] mb-14">
-              {fitur[0].subtitle}
+              {fitur[1].subtitle}
             </div>
 
-            <div className="text-[#1E1E1E] mb-56">
-              {fitur[0].content.map((val, index) => {
+            <div className="text-[#5C6978] mb-56">
+              {fitur[1].content.map((val, index) => {
                 return (
                   <div className="flex space-x-2 my-5" key={index}>
                     <UilCheckCircle color="#47BE7D" size="28" />
@@ -36,12 +36,12 @@ export default function CreditScoring() {
 
           {/* Col 2 */}
           <div>
-            <img src={fitur[0].image} alt="Credit Score" srcset="" />
+            <img src={fitur[1].image} alt="Credit Score" srcset="" />
           </div>
         </div>
       </div>
       <Subs />
       <Footer />
     </>
-  );
+  )
 }
