f1ea04ed845f1bed5b213b4e11c80fac59f7e600
AnonymusBukanSulton <zagzegzagzegzagzeg@gmail.com>
2022-08-24 13:29:04 +0700

    reverse awal

---
+++ Data.js
@@ -0,0 +1,116 @@
+export const testimoni =[
+    {
+        id:1,
+        image:"img/user testi 1.png",
+        name: "Martin Sinahaan",
+        job:"Pekerja Lepas",
+        review:"Ini adalah fenomenal, pembayaran yang terintegerasi, sistem pembelian pulsa dan PPOB yang lengkap",
+        star:5
+    },
+    {
+        id:2,
+        image:"img/user testi 2.png",
+        name: "Martin Sin",
+        job:"Pekerja Lepas",
+        review:"Saya Tidak dapat menjelaskan lagi bagaimana aplikasi ini sangat baik",
+        star:4
+    },
+    {
+        id:3,
+        image:"img/user testi 3.png",
+        name: "Martin Sintak",
+        job:"Pekerja Lepas",
+        review:"Semua telah terdigitalisasi dan cashless saya sangat suka itu semoga dimasa depan lebih simple lagi",
+        star:3
+    }
+]
+
+export const fitur = [
+    {
+        id: 1,
+        image: "../img/credit scoring.png",
+        header: "Credit Scoring",
+        title: "Apa yang ditawarkan fitur Credit Scoring?",
+        subtitle: "Pelayanan untuk mengecek tingkat kelayakan kredit untuk mendapatkan produk seperti Kredit Tanpa Agunan (KTA), Kredit Pemilikan Rumah (KPR), kartu kredit, dan lain-lain.",
+        content: ['Meningkatkan kualitas penilaian kelayakan kredit', 'Memperluas akses kredit', 'Mempercepat penyerapan kredit', 'Penyajian data yang lebih akurat dan real-time']
+    },
+    {
+        id: 2,
+        image: "../img/kyc.png",
+        header: "Verifikasi KYC",
+        title: "Apa yang ditawarkan fitur Verifikasi KYC?",
+        subtitle: "Identifikasi, verifikasi, dan pemantauan transaksi nasabah demi melindungi keamanan data dan kenyamanan layanan finansial Anda.",
+        content: ['Menghindari tindak kejahatan', 'Hemat biaya, cepat dan praktis', 'Menjamin keamanan nasabah', 'Paperless']
+    },
+    {
+        id: 3,
+        image: "../img/mobile banking.png",
+        header: "Mobile Banking",
+        title: 'Apa yang ditawarkan oleh M-Banking CREDIS?',
+        subtitle: "Cek saldo rekening, transfer dana, dan bayar tagihan Anda dengan lebih mudah dan nyaman melalui M-Banking CREDIS.",
+        content: ['Proses pembuatan rekening secara online', 'Tidak ada saldo minimum', 'Gratis biaya admin bulanan', 'Tampilan aplikasi ringan & mudah digunakan']
+    },
+    {
+        id: 4,
+        image: "../img/laporan terperinci.png",
+        header: "Laporan Terperinci",
+        title: 'Apa yang ditawarkan fitur Laporan Terperinci?',
+        subtitle: "Semua riwayat transfer, mutasi, dan sistem keuangan Anda direkap secara terperinci dan bisa di unduh.",
+        content: ['Cek mutasi mingguan atau bulanan', 'Cek riwayat detail transfer', 'Cek riwayat detail kredit', 'Dapat di unduh dalam format PDF']   
+    },
+]
+
+export const blog = [ 
+    {
+        id : 1,
+        image : "img/image blog 1.png",
+        header : "Apa itu Core Banking System? Berikut Penjelasannya",
+        content : "Core Banking system sendiri adalah jantung dari sebuah bank, karena sistem ini digunakan untuk memproses loan, dan di dalamnya tersimpan data nasabah serta semua transaksi sejak nasabah membuka rekening di bank sampai menutupnya.",
+        date : "Di Publish 21 Juli 2022"
+    },
+    {
+        id : 2,
+        image : "img/image blog 2.png",
+        header : "BI dan Kemenkeu Sepakati Pengembangan CBS",
+        content : "Bank Indonesia (BI) dan Kementerian Keuangan (Kemenkeu) menyepakati pengembangan aplikasi Core Banking System (CBS) oleh BI dan interkoneksi dengan sistem di Kemenkeu di tahun 2021, sebagai salah satu topik strategis.",
+        date : "Di Publish 7 Desember 2021"
+    },
+    {
+        id : 3,
+        image : "img/image blog 3.png",
+        header : "Integrasi Core Banking Permudah Layanan Perbankan",
+        content : "Seiring dengan semakin meningkatnya kebutuhan kecepatan dalam perbankan, core banking yang ada saat ini semakin berbenah dan ditingkatkan pula. Sistem pada perbankan konvensional saat ini.",
+        date : "Di Publish 24 November 2021"
+    },
+    {
+        id : 4,
+        image : "img/image blog 4.png",
+        header : "Sejumlah Bank Swasta Tingkatkan Core Banking System",
+        content : "Sejumlah bank umum swasta nasional (BUSN) berupaya meningkatkan core banking system mereka untuk meningkatkan pelayanan kepada nasabah dan memperkuat komptensi di bidang digital. Presiden Direktur PT Bank CIMB Niaga Tbk (CIMB Niaga) Tigor M Siahaan mengatakan, saat ini bank yang ia pimpin secara resmi menggunakan core banking system bernama 1Platform (1P).",
+        date : "Di Publish 24 Februari 2022"
+    }
+
+]
+
+export const mitra = [
+    {
+        id : 1,
+        image : "img/menantea.png"
+    },
+    {
+        id : 2,
+        image : "img/Logo OVO.png"
+    },
+    {
+        id : 3,
+        image : "img/Logo DANA.png"
+    },
+    {
+        id : 4,
+        image : "img/Honeywell.png"
+    },
+    {
+        id : 5,
+        image : "img/aws.png"
+    },
+]
