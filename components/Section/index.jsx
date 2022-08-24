2cb6822c642ee113b977b4035f4b21ed64bda1c9
faizalajiw <faizalajiwibowo7@gmail.com>
2022-08-24 20:24:19 +0700

    separate section

---
+++ components/Section/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Section = () => {
+  return (
+    <>
+      <div className=" text-center px-7 mb-20 text-black md:px-32 md:mb-40">
+        <h1 className=" text-3xl font-semibold mb-3 md:font-bold">
+          Mengapa Kami?
+        </h1>
+        <p className=" font-light md:font-normal">
+          CREDIS adalah aplikasi core banking system bagi bank umum, BPR,
+          koperasi dan finance. Kami menyediakan fitur seperti Credit Scoring,
+          Verifikasi KYC, Mobile Banking, Laporan Terperinci, dan lainnya.
+        </p>
+      </div>
+    </>
+  );
+};
