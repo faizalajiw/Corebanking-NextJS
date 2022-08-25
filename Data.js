7cf12b720cb3131ec11ee9bf4f9230d0268c3ef3
faizalajiw <faizalajiwibowo7@gmail.com>
2022-08-25 11:32:25 +0700

    tambah data Kebijakan Privasi

--- Data.js
+++ Data.js
@@ -1,116 +1,191 @@
-export const testimoni =[
-    {
-        id:1,
-        image:"img/user testi 1.png",
-        name: "Martin Sinahaan",
-        job:"Pekerja Lepas",
-        review:"Ini adalah fenomenal, pembayaran yang terintegerasi, sistem pembelian pulsa dan PPOB yang lengkap",
-        star:5
-    },
-    {
-        id:2,
-        image:"img/user testi 2.png",
-        name: "Martin Sin",
-        job:"Pekerja Lepas",
-        review:"Saya Tidak dapat menjelaskan lagi bagaimana aplikasi ini sangat baik",
-        star:4
-    },
-    {
-        id:3,
-        image:"img/user testi 3.png",
-        name: "Martin Sintak",
-        job:"Pekerja Lepas",
-        review:"Semua telah terdigitalisasi dan cashless saya sangat suka itu semoga dimasa depan lebih simple lagi",
-        star:3
-    }
-]
+export const testimoni = [
+  {
+    id: 1,
+    image: "img/user testi 1.png",
+    name: "Martin Sinahaan",
+    job: "Pekerja Lepas",
+    review:
+      "Ini adalah fenomenal, pembayaran yang terintegerasi, sistem pembelian pulsa dan PPOB yang lengkap",
+    star: 5,
+  },
+  {
+    id: 2,
+    image: "img/user testi 2.png",
+    name: "Martin Sin",
+    job: "Pekerja Lepas",
+    review:
+      "Saya Tidak dapat menjelaskan lagi bagaimana aplikasi ini sangat baik",
+    star: 4,
+  },
+  {
+    id: 3,
+    image: "img/user testi 3.png",
+    name: "Martin Sintak",
+    job: "Pekerja Lepas",
+    review:
+      "Semua telah terdigitalisasi dan cashless saya sangat suka itu semoga dimasa depan lebih simple lagi",
+    star: 3,
+  },
+];
 
 export const fitur = [
-    {
-        id: 1,
-        image: "../img/credit scoring.png",
-        header: "Credit Scoring",
-        title: "Apa yang ditawarkan fitur Credit Scoring?",
-        subtitle: "Pelayanan untuk mengecek tingkat kelayakan kredit untuk mendapatkan produk seperti Kredit Tanpa Agunan (KTA), Kredit Pemilikan Rumah (KPR), kartu kredit, dan lain-lain.",
-        content: ['Meningkatkan kualitas penilaian kelayakan kredit', 'Memperluas akses kredit', 'Mempercepat penyerapan kredit', 'Penyajian data yang lebih akurat dan real-time']
-    },
-    {
-        id: 2,
-        image: "../img/kyc.png",
-        header: "Verifikasi KYC",
-        title: "Apa yang ditawarkan fitur Verifikasi KYC?",
-        subtitle: "Identifikasi, verifikasi, dan pemantauan transaksi nasabah demi melindungi keamanan data dan kenyamanan layanan finansial Anda.",
-        content: ['Menghindari tindak kejahatan', 'Hemat biaya, cepat dan praktis', 'Menjamin keamanan nasabah', 'Paperless']
-    },
-    {
-        id: 3,
-        image: "../img/mobile banking.png",
-        header: "Mobile Banking",
-        title: 'Apa yang ditawarkan oleh M-Banking CREDIS?',
-        subtitle: "Cek saldo rekening, transfer dana, dan bayar tagihan Anda dengan lebih mudah dan nyaman melalui M-Banking CREDIS.",
-        content: ['Proses pembuatan rekening secara online', 'Tidak ada saldo minimum', 'Gratis biaya admin bulanan', 'Tampilan aplikasi ringan & mudah digunakan']
-    },
-    {
-        id: 4,
-        image: "../img/laporan terperinci.png",
-        header: "Laporan Terperinci",
-        title: 'Apa yang ditawarkan fitur Laporan Terperinci?',
-        subtitle: "Semua riwayat transfer, mutasi, dan sistem keuangan Anda direkap secara terperinci dan bisa di unduh.",
-        content: ['Cek mutasi mingguan atau bulanan', 'Cek riwayat detail transfer', 'Cek riwayat detail kredit', 'Dapat di unduh dalam format PDF']   
-    },
-]
+  {
+    id: 1,
+    image: "../img/credit scoring.png",
+    header: "Credit Scoring",
+    title: "Apa yang ditawarkan fitur Credit Scoring?",
+    subtitle:
+      "Pelayanan untuk mengecek tingkat kelayakan kredit untuk mendapatkan produk seperti Kredit Tanpa Agunan (KTA), Kredit Pemilikan Rumah (KPR), kartu kredit, dan lain-lain.",
+    content: [
+      "Meningkatkan kualitas penilaian kelayakan kredit",
+      "Memperluas akses kredit",
+      "Mempercepat penyerapan kredit",
+      "Penyajian data yang lebih akurat dan real-time",
+    ],
+  },
+  {
+    id: 2,
+    image: "../img/kyc.png",
+    header: "Verifikasi KYC",
+    title: "Apa yang ditawarkan fitur Verifikasi KYC?",
+    subtitle:
+      "Identifikasi, verifikasi, dan pemantauan transaksi nasabah demi melindungi keamanan data dan kenyamanan layanan finansial Anda.",
+    content: [
+      "Menghindari tindak kejahatan",
+      "Hemat biaya, cepat dan praktis",
+      "Menjamin keamanan nasabah",
+      "Paperless",
+    ],
+  },
+  {
+    id: 3,
+    image: "../img/mobile banking.png",
+    header: "Mobile Banking",
+    title: "Apa yang ditawarkan oleh M-Banking CREDIS?",
+    subtitle:
+      "Cek saldo rekening, transfer dana, dan bayar tagihan Anda dengan lebih mudah dan nyaman melalui M-Banking CREDIS.",
+    content: [
+      "Proses pembuatan rekening secara online",
+      "Tidak ada saldo minimum",
+      "Gratis biaya admin bulanan",
+      "Tampilan aplikasi ringan & mudah digunakan",
+    ],
+  },
+  {
+    id: 4,
+    image: "../img/laporan terperinci.png",
+    header: "Laporan Terperinci",
+    title: "Apa yang ditawarkan fitur Laporan Terperinci?",
+    subtitle:
+      "Semua riwayat transfer, mutasi, dan sistem keuangan Anda direkap secara terperinci dan bisa di unduh.",
+    content: [
+      "Cek mutasi mingguan atau bulanan",
+      "Cek riwayat detail transfer",
+      "Cek riwayat detail kredit",
+      "Dapat di unduh dalam format PDF",
+    ],
+  },
+];
 
-export const blog = [ 
-    {
-        id : 1,
-        image : "img/image blog 1.png",
-        header : "Apa itu Core Banking System? Berikut Penjelasannya",
-        content : "Core Banking system sendiri adalah jantung dari sebuah bank, karena sistem ini digunakan untuk memproses loan, dan di dalamnya tersimpan data nasabah serta semua transaksi sejak nasabah membuka rekening di bank sampai menutupnya.",
-        date : "Di Publish 21 Juli 2022"
-    },
-    {
-        id : 2,
-        image : "img/image blog 2.png",
-        header : "BI dan Kemenkeu Sepakati Pengembangan CBS",
-        content : "Bank Indonesia (BI) dan Kementerian Keuangan (Kemenkeu) menyepakati pengembangan aplikasi Core Banking System (CBS) oleh BI dan interkoneksi dengan sistem di Kemenkeu di tahun 2021, sebagai salah satu topik strategis.",
-        date : "Di Publish 7 Desember 2021"
-    },
-    {
-        id : 3,
-        image : "img/image blog 3.png",
-        header : "Integrasi Core Banking Permudah Layanan Perbankan",
-        content : "Seiring dengan semakin meningkatnya kebutuhan kecepatan dalam perbankan, core banking yang ada saat ini semakin berbenah dan ditingkatkan pula. Sistem pada perbankan konvensional saat ini.",
-        date : "Di Publish 24 November 2021"
-    },
-    {
-        id : 4,
-        image : "img/image blog 4.png",
-        header : "Sejumlah Bank Swasta Tingkatkan Core Banking System",
-        content : "Sejumlah bank umum swasta nasional (BUSN) berupaya meningkatkan core banking system mereka untuk meningkatkan pelayanan kepada nasabah dan memperkuat komptensi di bidang digital. Presiden Direktur PT Bank CIMB Niaga Tbk (CIMB Niaga) Tigor M Siahaan mengatakan, saat ini bank yang ia pimpin secara resmi menggunakan core banking system bernama 1Platform (1P).",
-        date : "Di Publish 24 Februari 2022"
-    }
-
-]
+export const blog = [
+  {
+    id: 1,
+    image: "img/image blog 1.png",
+    header: "Apa itu Core Banking System? Berikut Penjelasannya",
+    content:
+      "Core Banking system sendiri adalah jantung dari sebuah bank, karena sistem ini digunakan untuk memproses loan, dan di dalamnya tersimpan data nasabah serta semua transaksi sejak nasabah membuka rekening di bank sampai menutupnya.",
+    date: "Di Publish 21 Juli 2022",
+  },
+  {
+    id: 2,
+    image: "img/image blog 2.png",
+    header: "BI dan Kemenkeu Sepakati Pengembangan CBS",
+    content:
+      "Bank Indonesia (BI) dan Kementerian Keuangan (Kemenkeu) menyepakati pengembangan aplikasi Core Banking System (CBS) oleh BI dan interkoneksi dengan sistem di Kemenkeu di tahun 2021, sebagai salah satu topik strategis.",
+    date: "Di Publish 7 Desember 2021",
+  },
+  {
+    id: 3,
+    image: "img/image blog 3.png",
+    header: "Integrasi Core Banking Permudah Layanan Perbankan",
+    content:
+      "Seiring dengan semakin meningkatnya kebutuhan kecepatan dalam perbankan, core banking yang ada saat ini semakin berbenah dan ditingkatkan pula. Sistem pada perbankan konvensional saat ini.",
+    date: "Di Publish 24 November 2021",
+  },
+  {
+    id: 4,
+    image: "img/image blog 4.png",
+    header: "Sejumlah Bank Swasta Tingkatkan Core Banking System",
+    content:
+      "Sejumlah bank umum swasta nasional (BUSN) berupaya meningkatkan core banking system mereka untuk meningkatkan pelayanan kepada nasabah dan memperkuat komptensi di bidang digital. Presiden Direktur PT Bank CIMB Niaga Tbk (CIMB Niaga) Tigor M Siahaan mengatakan, saat ini bank yang ia pimpin secara resmi menggunakan core banking system bernama 1Platform (1P).",
+    date: "Di Publish 24 Februari 2022",
+  },
+];
 
 export const mitra = [
-    {
-        id : 1,
-        image : "img/menantea.png"
-    },
-    {
-        id : 2,
-        image : "img/Logo OVO.png"
-    },
-    {
-        id : 3,
-        image : "img/Logo DANA.png"
-    },
-    {
-        id : 4,
-        image : "img/Honeywell.png"
-    },
-    {
-        id : 5,
-        image : "img/aws.png"
-    },
-]
+  {
+    id: 1,
+    image: "img/menantea.png",
+  },
+  {
+    id: 2,
+    image: "img/Logo OVO.png",
+  },
+  {
+    id: 3,
+    image: "img/Logo DANA.png",
+  },
+  {
+    id: 4,
+    image: "img/Honeywell.png",
+  },
+  {
+    id: 5,
+    image: "img/aws.png",
+  },
+];
+
+export const kebijakan = [
+  {
+    id: 1,
+    title: "Kebijakan Privasi Credis",
+    content:
+      "Kami adalah pemilik konten dan operator situs web http://www.redis.id, situs terkait dan microsites yang diakses melalui situs web tersebut (masing-masing disebut “Situs“ dan secara kolektif disebut “Situs-situs“) (“Credis.“, atau “kami“). Harap diperhatikan bahwa Credis. tidak bertanggung jawab atas situs web lain yang dikelola oleh perusahaan lain dari grup Credis. atau tautan ke situs atau konten lain di internet yang dimiliki atau dioperasikan oleh pihak ketiga. Situs atau konten terkait tidak berada di bawah kendali kami dan Credis. tidak bertanggung jawab atas kesalahan, kelalaian, penundaan, pencemaran nama baik, fitnah, kebohongan, pornografi, konten cabul, ketidakakuratan atau materi lain yang terkandung dalam konten, atau konsekuensi dari mengakses situs web terkait.",
+  },
+  {
+    id: 2,
+    title: "Pernyataan Privasi",
+    content:
+      "Melindungi privasi Anda sangat penting bagi Credis. Kami menghargai privasi Anda dan kami berkomitmen untuk melindungi setiap dan semua informasi pribadi yang mungkin diberikan oleh Anda dan oleh karena itu akan memastikan perlakuan yang adil, keandalan, kerahasiaan, dan keamanan informasi pribadi Anda. Kami merancang Kebijakan Privasi ini untuk menjelaskan kepada Anda bagaimana kami mengumpulkan, menggunakan, dan membagikan informasi pribadi Anda. “Informasi pribadi” berarti informasi apa pun yang dapat digunakan untuk mengidentifikasi Anda secara pribadi (tidak termasuk informasi apa pun yang telah dikumpulkan atau dibuat anonim) dan sebagaimana ditafsirkan berdasarkan Peraturan Menteri Komunikasi dan Teknologi Informasi No. 20 tahun 2016 tentang Perlindungan Data Pribadi dalam Sistem Elektronik (selanjutnya disebut sebagai “Peraturan 20/2016“).\\n\\nInformasi yang terdapat dalam Kebijakan Privasi ini telah dipersiapkan sehubungan dan sesuai dengan peraturan yang berlaku dan untuk digunakan khusus oleh Credis. dan afiliasinya dan/atau penerima pengalihannya.",
+  },
+  {
+    id: 3,
+    title: "Penerimaan dan Persetujuan Anda",
+    content:
+      "Anda wajib membaca Kebijakan Privasi kami dari waktu ke waktu. Dengan mengakses atau menggunakan layanan atau produk kami serta Situs Web kami, hal tersebut akan dianggap sebagai persetujuan dan penerimaan Anda terhadap Kebijakan Privasi kami, dengan demikian, Anda setuju untuk terikat pada setiap ketentuan yang tercantum dalam Kebijakan Privasi kami. Jika Anda tidak setuju dengan ketentuan atau ketentuan apa pun dalam Kebijakan Privasi ini, jangan gunakan layanan atau produk kami serta Situs Web kami. [Dengan mengirimkan pemberitahuan ke email Anda], kami memiliki hak untuk merevisi dan/atau memperbarui Kebijakan Privasi kami dari waktu ke waktu dan Anda setuju untuk terikat dengan Kebijakan Privasi yang direvisi dan/atau diperbarui tersebut. Jika Anda tidak setuju dengan ketentuan kami yang direvisi dan/atau diperbarui dalam Kebijakan Privasi, silakan berhenti mengakses layanan atau produk kami atau Situs Web kami.\\n\\nPenerimaan dan persetujuan Anda terhadap Kebijakan Privasi ini juga akan berkaitan dengan ketentuan yang tercantum berdasarkan Ketentuan Layanan dan akan dianggap dan tidak dapat dipisahkan dan satu perjanjian yang mengatur hak dan kewajiban kami kepada Anda.",
+  },
+  {
+    id: 4,
+    title: "Pengumpulan Informasi Pribadi Anda",
+    content:
+      "Kami mengumpulkan dan mengelola informasi pribadi Anda ketika Anda mendaftar untuk akun Credis. sebagai bagian dari proses verifikasi identitas kami, dalam proses transaksi, atau ketika Anda meminta tanda terima digital. Kami dapat mengumpulkan dan mengelola, tetapi tidak terbatas pada pengumpulan dan pengelolaan, sebagai berikut:\\n\\n1. Informasi dasar pengguna, seperti nama lengkap Anda, alamat email, alamat tempat tinggal dan tempat tinggal tetap, nomor telepon, tanggal lahir, usia, rincian rekening bank,\\n2. informasi pekerjaan, kartu kredit, dan / atau informasi rekening keuangan; Informasi apa pun yang diunggah oleh Anda atau informasi apa pun tentang produk dan layanan Anda;\\n3. Informasi yang melekat pada Anda saat Anda mengakses Situs kami;\\n4. Informasi apa pun di komputer, ponsel, atau perangkat lain tempat Anda memasang aplikasi kami atau mengakses Layanan kami, yang akan bergantung pada izin yang Anda berikan;\\n5. Setiap informasi yang dikumpulkan oleh cookies, suar web, serta informasi log umum;\\n\\nKami berhak, dari waktu ke waktu, melakukan verifikasi terhadap informasi pribadi yang Anda berikan kepada kami, dengan mengirimkan surat verifikasi, e-mail, atau mengharuskan Anda untuk mengirimkan dokumentasi pendukung, atau cara apa pun, sebagaimana yang diminta.\\nAnda dengan ini setuju bahwa Informasi Pribadi Anda dapat diungkapkan kepada pihak-pihak sebagai berikut:\\n- afiliasi dan anak perusahaan, agen dan subkontraktor Perusahaan;\\n- pemerintah, badan pengatur, dan lembaga pencegah penipuan untuk tujuan mengidentifikasi, mencegah, mendeteksi atau menanggulangi penipuan, pencucian uang, atau kejahatan lainnya, dan untuk tujuan yang sah lainnya;\\n- kepada pihak ketiga untuk tujuan manajemen risiko\\n- entitas lain yang mungkin diwajibkan oleh hukum atau sebagai kepentingan publik dapat menjamin, untuk tujuan yang dijelaskan dalam paragraf berikutnya; dan\\n- pembeli pada saat terjadinya akuisisi. Anda memahami dan menyadari bahwa pengungkapan tersebut harus dibuat dan setuju untuk mengesampingkan hak Anda untuk mengajukan klaim ganti rugi, sejauh diizinkan oleh undang-undang.",
+  },
+  {
+    id: 5,
+    title: "Kerahasiaan Akun Anda",
+    content:
+      "Anda harus bertanggung jawab untuk menjaga kerahasiaan akun dan kata sandi Anda sendiri, serta setiap dan semua aplikasi yang diajukan, kewajiban yang disetujui atau dimasukkan ke dalam dan semua kegiatan lain yang dilakukan berdasarkan akun tersebut. Anda setuju untuk segera memberi tahu kami tentang penggunaan atau pengungkapan yang tidak sah atas akun atau kata sandi Anda, setiap kegiatan tidak sah di bawah akun Anda atau pelanggaran keamanan lainnya.\\n\\nKami tidak akan bertanggung jawab atas kerugian apa pun yang mungkin Anda alami akibat penggunaan pihak ketiga atas akun atau kata sandi Anda, baik dengan atau tanpa sepengetahuan Anda, tanpa kesalahan atau kelalaian dari pihak kami.",
+  },
+  {
+    id: 6,
+    title: "Hukum yang Berlaku",
+    content:
+      "Setiap referensi terhadap hukum dan peraturan yang berlaku berdasarkan Kebijakan Privasi ini harus ditafsirkan sesuai dengan ketentuan yang tercantum di bawah hukum dan peraturan Republik Indonesia.",
+  },
+  {
+    id: 7,
+    title: "Hubungi kami",
+    content:
+      "Untuk pertanyaan, kekhawatiran atau klarifikasi apapun tentang Kebijakan Privasi kami, kami dapat dihubungi melalui sistem dukungan pelanggan di 0821654221 dan/atau email ke hellocredis@gmail.com.",
+  },
+];
