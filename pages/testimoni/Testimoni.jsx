f73b159b109d0050e64043df9009ccf9634a2bde
Sultoni, romadhon <sultoniromadhan16@gmail.com>
2022-08-30 19:33:28 +0700

    Testimoni Navbar

--- pages/testimoni/Testimoni.jsx
+++ pages/testimoni/Testimoni.jsx
@@ -9,8 +9,8 @@ export default function Testimoni() {
   return (
     <div>
       <Header />
-      <div className=" text-center px-7 my-20 text-black md:py-5 md:px-32">
-        <h1 className=" text-3xl font-semibold mb-3 md:font-bold">Testimoni</h1>
+      <div className=" text-center px-7 text-black md:py-5 md:px-32">
+        <h1 className=" text-3xl font-semibold mb-3 md:font-bold md:pt-32">Testimoni</h1>
         <p className=" font-light md:font-normal">
           Apa yang dikatakan pelanggan mengenai kami
         </p>
